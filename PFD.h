266137b1d3320a3d2845df2e20369c5ca3741c7e
rdspring <rdspring1@gmail.com>
2013-06-19 15:47:51 -0500

    Passed Sphere PFD example

--- PFD.h
+++ PFD.h
@@ -11,14 +11,13 @@
 
 #include <cassert>  // assert
 #include <iostream> // endl, istream, ostream
-#include <queue>
+
 #define MAX_SIZE 6
 using namespace std;
 // graph representing the partial ordering
 int pfd_graph[MAX_SIZE][MAX_SIZE];
-int initial_total_task;
-int task_left;
-//priority_queue<int, vector<int>, greater<int>> min_heap;
+int initial_total_task = 0;
+int task_left = -1;
 // ------------
 // pfd_read
 // ------------
@@ -41,29 +40,21 @@ bool pfd_read (std::istream& input)
     input >> task_left;
     initial_total_task = task_left;
     input >> rule_count;
-    cout<<"a\n";
     for(int i = 0; i < rule_count; i++)
     {
-        cout<<"i: "<< i << "\n";
         int current_vertex, dependencies;
         input >> current_vertex;
         input >> dependencies;
-        cout<<"dep " << dependencies;
         pfd_graph[current_vertex][0] = dependencies;
         for(int j = 1; j <= dependencies; j++)
         {
-            cout << "comes Here";
-            cout << "cv: " << current_vertex;
-            
             input >> pfd_graph[current_vertex][j];
-            cout<<"j: " << j;
         }
     }
-    cout<<"true";
     return true;
 }
 
-void print_crazy()
+void print_graph()
 {
     for(int i = 0; i < MAX_SIZE; i++)
     {
@@ -75,44 +66,27 @@ void print_crazy()
     }
 }
 
-// -------------
-// collatz_print
-// -------------
-
-/**
- * prints the values of range1, range2, and cycle_count
- * @param output a std::ostream
- * @param range1 the beginning of the range, inclusive
- * @param range2 the end       of the range, inclusive
- * @param cycle_count the max cycle length
- */
-void pfd_print (std::ostream& output, int range1, int range2, int cycle_count) {
-    assert(range1 > 0);
-    assert(range2 > 0);
-    assert(range1 < 1000000);
-    assert(range2 < 1000000);
-    assert(cycle_count > 0);
-    output << range1 << " " << range2 << " " << cycle_count << std::endl;}
-
-void pfd_clean(int removed_vertex)
+void pfd_clean(int needClean[])
 {
-    for(int i = 1; i < initial_total_task; i++)
+    for(int i = 1; i <= initial_total_task; i++)
     {
-        if(pfd_graph[i][0] == 0 || pfd_graph[i][0] == initial_value || pfd_graph[i][0] == pass)
+        if((pfd_graph[i][0] == 0) || (pfd_graph[i][0] == initial_value) || (pfd_graph[i][0] == pass))
         {
             continue;
         }
         for(int j = 1; j < MAX_SIZE; j++)
         {
+//            cout << "THIS IS I: " << i << "\n";
+ //           cout << pfd_graph[i][j] << "t\n";
             if(pfd_graph[i][j] == initial_value)
             {
                 break;
             }
-            else if(pfd_graph[i][j] == removed_vertex)
+            else if(needClean[pfd_graph[i][j]] == 1 )
             {
+   //             cout << pfd_graph[i][j] <<"\n";
                 pfd_graph[i][j] = 0;
                 pfd_graph[i][0]--;
-                break;
             }
         }
     }
@@ -121,18 +95,25 @@ void pfd_clean(int removed_vertex)
 
 void pfd_remove()
 {
+    int needClean[MAX_SIZE] = {0};
     for(int i = 1; i <= initial_total_task; i++)
     {
-//        cout << i;
+   //     cout << pfd_graph[i][0] << "\n";
         if((pfd_graph[i][0] == initial_value) || (pfd_graph[i][0] == 0))
         {
-            cout << i;
             cout << i << " ";
             task_left--;
+ //           cout << "taskleft: " <<task_left<<"\n";
             pfd_graph[i][0] = pass;
-            pfd_clean(i);
+            needClean[i] = 1;
+        }
+        else if(pfd_graph[i][0] == pass)
+        {
+            continue;
         }
     }    
+    pfd_clean(needClean);
+
 }
 
 // -------------
@@ -153,23 +134,16 @@ void pfd_solve (std::istream& input, std::ostream& output)
             pfd_graph[i][j] = -1;
         }
     }
-cout << "1\n";        
     if(!pfd_read(input))
     {
         output << "empty input stream";
     }
-    cout<<"2\n";
- print_crazy(); 
- cout <<"3\n";  
-    while(task_left > 0)
+//    print_graph();
+   while(task_left > 0)
     {
- //       cout << task_left;
         pfd_remove();
     }
-
-//    const int cycle_count = collatz_eval(range1, range2);
-//    assert(cycle_count > 0);
-//    pfd_print(output, range1, range2, cycle_count);
+         
 }
 
 
